57e9a5fe54fee8afc793d6c6b989e0034f254374
KooKoo <KooKooKitty92928@gmail.com>
2026-07-20 13:48:26 -0500

    Delete console commands file

--- ConsoleCommands.cs
+++
@@ -1,9 +0,0 @@
-namespace HoverDragon;
-
-public static class ConsoleCommands
-{
-    public static void Echo(params string[] s)
-    {
-        ErrorMessage.AddMessage(string.Join(" ", s));
-    }
-}
